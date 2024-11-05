4e00462004661161ea72ec1ba8cf90cb18a25de5
n754z265 <n754z265@JB-205-LAB27>
2024-11-05 14:06:04 -0600

    Added code to delete objects and rewrote code using unique pointers

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -2,6 +2,7 @@
 #include "Circle.h"
 #include "Shape.h"
 #include <vector>
+#include <memory>
 
 
 static void showShape(Shape& shape) {
@@ -9,19 +10,34 @@ static void showShape(Shape& shape) {
 }
 
 int main() {
+//Without Unique Pointer
 	unsigned int i;
 	vector<Shape*> shapeList;
-	Rectangle* rectanglePtr;
-	Circle* circlePtr;
 
-	rectanglePtr = new Rectangle;
-	circlePtr = new Circle;
-	shapeList.push_back(rectanglePtr);
-	shapeList.push_back(circlePtr);
+	shapeList.push_back(new Rectangle());
+	shapeList.push_back(new Circle());
 
-	for (i = 0; i < shapeList.size(); ++i) {
-		shapeList.at(i)->draw();
+	for (const auto& Shape : shapeList) {
+		Shape->draw();
 	}
+	//Delete objects when done
+	for (const auto& Shape : shapeList) {
+		delete Shape;
+	}
+
+	//Remove/Clear the vector
+	shapeList.clear();
+
+//With Unique Pointer
+	vector<std::unique_ptr<Shape>>shapeList2;
+	shapeList2.push_back(std::make_unique<Rectangle>(2,3));
+	shapeList2.push_back(std::make_unique<Circle>(4));
+
+	for (const auto& Shape : shapeList2) {
+		Shape->draw();
+	}
+
+	shapeList2.clear();
 
 	return 0;
 }
